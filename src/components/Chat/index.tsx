21853c9f2fd7d5408542201b1a16ff3b6d2268a1
siegaii <siiegaiii@gmail.com>
2023-11-01 11:19:49 +0800

    feat:bak

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable vue/multi-word-component-names */
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'ChartHead',
+  name: 'Chat',
   props: {},
   setup() {
-    return <div>1</div>
+    return ()=> <div>1123123123</div>
   }
 })
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Chat from '@/components/Chat'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,7 @@ const router = createRouter({
     {
       path: '/chat',
       name: 'chat',
-      component: HomeView
+      component: Chat
     },
     {
       path: '/about',
