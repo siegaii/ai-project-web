23e51fa745e16aa70071e7c1124faca5c3880820
siegaii <siiegaiii@gmail.com>
2023-11-02 15:00:08 +0800

    feat: chatContent

--- src/components/Chat/ChatContainer/index.tsx
+++ src/components/Chat/ChatContainer/index.tsx
@@ -4,14 +4,14 @@ import { defineComponent, ref } from 'vue'
 
 const roles = [
   {
-    id: '1',
-    name: '法律顾问',
-    avatar: 'https://afu-1255830993.cos.ap-shanghai.myqcloud.com/chato_image/Chato.png'
-  },
-  {
     id: '2',
     name: 'AI 智能客服',
     avatar: 'https://afu-1255830993.cos.ap-shanghai.myqcloud.com/447479457016221696.png'
+  },
+  {
+    id: '1',
+    name: '法律顾问',
+    avatar: 'https://afu-1255830993.cos.ap-shanghai.myqcloud.com/chato_image/Chato.png'
   },
   {
     id: '3',
@@ -28,11 +28,11 @@ export default defineComponent({
       <div class="flex h-full">
         <div class="w-[240px]" style={{ 'border-right': '1px solid rgb(228, 231, 237)' }}>
           <ChatList data={roles} defaultActive={active.value} onClick={(item)=> {
-            active.value = item.id
+            active.value = item
           }} />
         </div>
         <div class="w-full">
-          <ChatContent />
+          <ChatContent key={active.value.id} aiBot={active.value}/>
         </div>
       </div>
     )
--- src/components/Chat/ChatContent/index.tsx
+++ src/components/Chat/ChatContent/index.tsx
@@ -1,21 +1,26 @@
 import { ElAvatar } from 'element-plus'
-import { defineComponent } from 'vue'
-
-const itemMock = {
-  name: '法律顾问',
-  avatar: 'https://afu-1255830993.cos.ap-shanghai.myqcloud.com/chato_image/Chato.png'
-}
+import { defineComponent, type PropType } from 'vue'
+import type { ChatRole } from '../type'
 
 export default defineComponent({
   name: 'ChatContent',
-  setup() {
+  props: {
+    aiBot: {
+      type: Object as PropType<ChatRole>,
+      required: true
+    }
+  },
+  setup({ aiBot }) {
     return () => (
       <div class="flex">
-        <div class="flex w-full justify-center items-center p-3.5" style={{'border-bottom': '1px solid rgb(238, 238, 238)'}}>
+        <div
+          class="flex w-full justify-center items-center p-3.5"
+          style={{ 'border-bottom': '1px solid rgb(238, 238, 238)' }}
+        >
           <div class="w-[32px] mr-2">
-            <ElAvatar shape="square" size="small" src={itemMock.avatar} />
+            <ElAvatar shape="square" size="small" src={aiBot.avatar} />
           </div>
-          <div>{itemMock.name}</div>
+          <div>{aiBot.name}</div>
         </div>
         <div></div>
       </div>
